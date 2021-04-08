1651915d68f5e66cb527d0f5cb98037ee5cad168
Ola Kulykova <olesya.kulykova@gmail.com>
2021-04-08 15:24:53 +0200

    raw

--- game.js
+++ game.js
@@ -70,6 +70,7 @@ function movePacman(event) {
     switch(event.keyCode) {
         // LEFT
       case 37:
+        
         if(currentIndex % width !== 0 &&
         !squares[currentIndex - 1].classList.contains('ghost-lair') &&
         !squares[currentIndex - 1].classList.contains('wall'))
@@ -80,6 +81,7 @@ function movePacman(event) {
     
         // UP
       case 38:
+        
         if(currentIndex - width >= 0 &&
         !squares[currentIndex - width].classList.contains('ghost-lair') &&
         !squares[currentIndex - width].classList.contains('wall')) 
@@ -167,19 +169,25 @@ function moveGhost(ghost) {
 
     ghost.timerId = setInterval(function() {
         if  (!squares[ghost.ghostCurrentIndex + direction].classList.contains('ghost') &&
-          !squares[ghost.ghostCurrentIndex + direction].classList.contains('wall')) {
+          !squares[ghost.ghostCurrentIndex + direction].classList.contains('wall') ) {
             squares[ghost.ghostCurrentIndex].classList.remove(ghost.name, 'ghost', 'ghost-panic');
             ghost.ghostCurrentIndex += direction;
             squares[ghost.ghostCurrentIndex].classList.add(ghost.name, 'ghost');
+        } else if (squares[ghost.ghostCurrentIndex + direction].classList.contains('pac-dot')) {
+            squares[ghost.ghostCurrentIndex].classList.remove('pac-dot');
+            squares[ghost.ghostCurrentIndex].classList.add(ghost.name, 'ghost');
+            ghost.ghostCurrentIndex += direction;
+            squares[ghost.ghostCurrentIndex].classList.add('pac-dot');
+            
+            
         } else direction = directions[Math.floor(Math.random() * directions.length)];
-  
-    
+            
 
         if (ghost.panicMode === true) {
             squares[ghost.ghostCurrentIndex].classList.add('ghost-panic');
         }
         
-        if (ghost.panicMode && squares[ghost.ghostCurrentIndex].classList.contains('pac-man')) {
+        if (ghost.panicMode && squares[ghost.ghostCurrentIndex].classList.contains('pac-man', 'pac-up', 'pac-down', 'pac-left')) {
             squares[ghost.ghostCurrentIndex].classList.remove(ghost.name, 'ghost', 'ghost-panic');
             ghost.ghostCurrentIndex = ghost.startIndex;
             score +=100;
